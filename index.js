9df508126a5b3671efc48560a09f2f6e9102ccee
Josh Holloway <josh@threadster.co.uk>
2014-07-09 10:54:14 +0100

    Removed the assigning of the data key and removed the now redundant msg parameter; Now follows Boom like for like

--- index.js
+++ index.js
@@ -3,23 +3,19 @@ var boom = require('boom');
 module.exports = function () {
   return function (req, res, next) {
     if (res.boom) throw new Error('boom already exists on response object');
-    
+
     res.boom = {};
-    
+
     Object.keys(boom).forEach(function (key) {
       if (typeof boom[key] !== 'function') return;
-      
-      res.boom[key] = function (msg) {
-        var boomed = boom[key].apply(this, arguments);
 
-        if (boomed.data) {
-          boomed.output.payload.data = boomed.data;
-        }
+      res.boom[key] = function () {
+        var boomed = boom[key].apply(this, arguments);
 
         res.send(boomed.output.statusCode, boomed.output.payload);
       };
     });
-    
+
     next();
   };
 };
